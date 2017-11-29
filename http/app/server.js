cc19b8f34dd3ffbd0c4f71c4f727480e7612df95
1073317235 <1073317235@qq.com>
2017-11-29 23:49:52 +0800

    node

--- http/app/server.js
+++ http/app/server.js
@@ -7,32 +7,70 @@ const { types } = require('./content-type');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-	const pathname = url.parse(req.url).pathname;
-	const realPath = path.join(__dirname, 'client', pathname);
-	let ext = path.extname(realPath);
-	ext = ext ? ext.slice(1) : 'unknown';
-	fs.readFile(realPath, (err, data) => {
-		if (err) {
-			res.writeHead(404, {
-				'Content-Type': 'text/html'
-			});
-			res.write('<!doctype html>\n');
-			res.write('<title>404 Not Found</title>\n');
-			res.write('<h1>Not Found</h1>');
-			res.write(
-				'<p>The requested URL ' +
-				pathname + 
-				' was not found on this server.</p>'
-			);
-			res.end();
-		}else {
-			const contentType = types[ext] || "text/plain";
-            res.writeHead(200, {
-                'Content-Type': contentType
-            });
-            // res.write(data, "binary");
-            res.end(data, "binary");
-		}
-	});
+	req.setEncoding('utf8');  //对请求对象指定编码
+	const pathname = url.parse(req.url).pathname;  //获取请求地址
+
+	// console.log(url.parse(req.url));
+
+	if (pathname.search(/\.\w+/) !== -1) {  // 静态文件(以有后缀名为根据)
+		const realPath = path.join(__dirname, 'client', pathname);  //转化为文件所在的磁盘路径
+		let ext = path.extname(realPath);
+		ext = ext ? ext.slice(1) : 'unknown';
+		fs.readFile(realPath, (err, data) => {
+			if (err) {
+				res.writeHead(404, {
+					'Content-Type': 'text/html'
+				});
+				res.write(`<!doctype html>\n`);
+				res.write(`<title>404 Not Found</title>\n`);
+				res.write(`<h1>Not Found</h1>`);
+				res.write(`<p>The requested URL ${pathname} was not found on this server.</p>`);
+				res.write(`<p>error: ${JSON.stringify(err)}</p>`);
+				res.end();
+			}else {
+				const contentType = types[ext] || "text/plain";
+	            res.writeHead(200, {
+	                'Content-Type': contentType
+	            });
+	            // res.write(data, "binary");
+	            res.end(data, "binary");
+			}
+		});		
+	}else {  // ajax接口(相反，以没有后缀名为根据)
+		ajaxInterface(pathname, req, res);
+	}
+
 });
 server.listen(PORT);
+
+
+
+function ajaxInterface (pathname, req, res) {
+	var iname = pathname.slice(1);
+	console.log(iname);
+	switch (iname) {
+		case 'indexData':
+			_wh(200);
+			const data = {
+				status: 'success',
+				code: 2,
+				data: {
+					xxx: 'xxx',
+					yyy: 'yyy'
+				}
+			};
+			res.end(JSON.stringify(data));
+			break;
+		case 'aboutData':
+			_wh(200);
+			res.end('aboutData');
+			break;
+		default:
+			_wh(500);
+			res.end('The interface does not exist');
+			break;
+	}
+	function _wh (statusCode) {
+		res.writeHead(statusCode, {'Content-Type': 'application/json'});
+	}
+}
