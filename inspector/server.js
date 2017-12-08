a4609248dc353da46eadbefebc66541c699c80b2
1073317235 <1073317235@qq.com>
2017-12-08 23:22:05 +0800

    x

--- inspector/server.js
+++ inspector/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
-
+const port = 4000;
+debugger;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
